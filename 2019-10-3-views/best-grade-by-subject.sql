2d38f1938b11c440438697b0c7c598fa38c7873a
Mihail Kirov <mihail.kirov2@gmail.com>
2019-10-17 16:35:08 +0300

    view classwork

--- 2019-10-3-views/best-grade-by-subject.sql
+++ 2019-10-3-views/best-grade-by-subject.sql
@@ -1,16 +1,15 @@
 USE school;
-# CREATE VIEW BestMarksBySubject(
-# 	Class,
-# 	SubjectName,
-# 	AverageMark
-# )
-# AS
+CREATE VIEW BestMarksBySubject(
+	Class,
+	SubjectName,
+	AverageMark
+)
+AS
 SELECT st.Name, sb.Name, AVG(m.Mark)
 FROM StudentMarks m
 LEFT JOIN Students st
 ON m.StudentId = st.Id
 LEFT JOIN Subjects sb
 ON sb.Id = m.SubjectId
-group by sb.Id, sb.Name, st.Name
+group by sb.Id, sb.Name, st.Name, st.Id
 order by AVG(m.Mark) desc;
-#GROUP BY st.Id, sb.Id, sb.Name, st.Name;
